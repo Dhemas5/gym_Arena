bbc9ae62ac1b00933c96733767053878b792950b
acaca6 <chndrana06@gmail.com>
2025-11-05 21:45:26 +0700

    Pisah Tampilan Landing dan Ubah menjadi php

---
+++ landing page/header.php
@@ -0,0 +1,29 @@
+<!-- NAVBAR -->
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="#home">
+      <span class="brand-box">AF</span>
+      <div>
+        <span class="brand-text">Arena FIT</span>
+        <span class="brand-subtitle">Gym and Class</span>
+      </div>
+    </a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item"><a class="nav-link" href="#home">Home</a></li>
+        <li class="nav-item"><a class="nav-link" href="#about">Tentang Kami</a></li>
+        <li class="nav-item"><a class="nav-link" href="#classes">Kelas & Jadwal</a></li>
+        <li class="nav-item"><a class="nav-link" href="#membership">Membership</a></li>
+        <li class="nav-item"><a class="nav-link" href="#trainers">Instruktur</a></li>
+        <li class="nav-item"><a class="nav-link" href="#blog">Blog</a></li>
+        <li class="nav-item"><a class="nav-link" href="#contact">Kontak</a></li>
+      </ul>
+      <a href="registration.php" class="btn btn-danger ms-3">Daftar Member</a>
+    </div>
+  </div>
+</nav>
